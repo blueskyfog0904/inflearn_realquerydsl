4d7cc0a922921e5b63d3fc461e257a1a7f75272e
blueskyfog0904 <blueskyfog@gmail.com>
2023-07-19 10:30:19 +0900

    인프런 강좌 실전! QueryDsl 1~3 기본문법 > 집합까지

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,42 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Commit  // 테스트 케이스에서 DB에 데이터 남길 수 있음
+class QuerydslApplicationTests {
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
